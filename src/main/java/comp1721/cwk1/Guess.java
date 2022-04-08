b62f1e32fe35142b0b1a98f1ff4ee4b17d427e36
sc20jx <sc20jx@leeds.ac.uk>
2022-04-08 18:07:12 +0800

    Finish Guess.java and pass the GuessTest test successfully

--- src/main/java/comp1721/cwk1/Guess.java
+++ src/main/java/comp1721/cwk1/Guess.java
@@ -3,6 +3,8 @@ package comp1721.cwk1;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Guess {
@@ -18,7 +20,7 @@ public class Guess {
       guessNumber=num;
     }
     else{
-      throw new GameException("Invalid num!\n");
+      throw new GameException("Invalid choice!\n");
     }
   }
 
@@ -26,21 +28,35 @@ public class Guess {
 
   // TODO: Implement constructor with int and String parameters
   public Guess(int num,String word){
-    //guessNumber,chosen word
+    //validate guessNumber
     if((num<=6) &&(num>=1)){
         guessNumber=num;
     }
     else{
-      throw new GameException("Invalid num!\n");
+      throw new GameException("Invalid choice!\n");
     }
-    if(word.length()==5){
-      word.toUpperCase();
-      chosenWord=word;
+    //validate word length and type
+    if(word.length()==5) {
+      int i = 0;
+      int m = 0;
+      for (i = 0; i < 5; i++) {
+        int chr = word.charAt(i);
+        if ((chr < 65 || chr > 90) && (chr < 97 || chr > 122)) {
+          System.out.println(chr);
+          m++;
+        }
+        if (m > 0) {
+          throw new GameException("Invalid choice!\n");
+        } else {
+          chosenWord = word.toUpperCase();
+        }
+      }
     }
     else{
-      throw new GameException("Invalid words!\n");
+      throw new GameException("Invalid choice!\n");
     }
-    }
+
+  }
 
 
 
@@ -60,41 +76,41 @@ public class Guess {
   public void readFromPlayer(){
     //if Guess object is created without initializing the chosen word,
     // this can obtain the word from the player
-
+    chosenWord=INPUT.nextLine();
   }
 
 
 
   // TODO: Implement compareWith(), giving it a String parameter and String return type
-//target是答案，chosen是用户输入
+
   public String compareWith(String target){
-    //return a string
-    //use ANSI escape codes to 标记颜色
-    //green:letter+position right
-    //yellow:letter
-    //white:letter not
-    target.toUpperCase();
+    target=target.toUpperCase();
     char[] tarArray=target.toCharArray();
     char[] choArray=chosenWord.toCharArray();
     String[] choout=new String[5];
     //List<String> choArray_out=new ArrayList<>();
     int i=0;
     int j=0;
+    int m=0;
+    int temp=0;
+    for(m=0;m<choArray.length;m++){
+      choout[m]="\033[30;107m "+choArray[m]+" \033[0m";
+    }
     for(i=0;i<choArray.length;i++) {
       for (j = 0; j < tarArray.length; j++) {
         if (choArray[i] == tarArray[i]) {
-          choout[i] = "\033[30;102m " + choArray[i] + "  \\033[0m";
+          choout[i] = "\033[30;102m " + choArray[i] + " \033[0m";
         }
         if (choArray[i] == tarArray[j] && i != j) {
-          choout[i] = "\033[30;103m " + choArray[i] + "  \\033[0m";
+          choout[i] = "\033[30;103m " + choArray[i] + " \033[0m";
+          for(temp=0;temp<i;temp++){
+            if(choArray[i]==choArray[temp]){
+              choout[i]="\033[30;107m "+choArray[i]+" \033[0m";
+            }
+          }
         }
       }
     }
-//
-//        else{
-//          choout[i]="\033[30;107m "+choArray[i]+"  \\033[0m";
-//        }
-//      }
 
     String output = choout[0] + choout[1] + choout[2] + choout[3] + choout[4];
     return output;
@@ -103,6 +119,12 @@ public class Guess {
   public boolean matches(String target){//evaluate player's guess
     //return true if the player's chosen word matches the target one
     //return false if doesn't match
-    return true;
+    target=target.toUpperCase();
+    if(target.equals(chosenWord)){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 }
